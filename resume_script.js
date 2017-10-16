4c4a696111b2708a69a8e6c621243383f19b0cd8
Ann Fang <32095875+e24033150@users.noreply.github.com>
2017-10-16 21:43:14 +0800

    Add files via upload

---
+++ resume_script.js
@@ -0,0 +1,12 @@
+$(function() {
+	
+	$('#Ann').on('mouseover', function() {
+		$(this).css(
+		"color","#FFE4B5" );
+	});
+	$('#Ann').on('mouseout', function(){
+		$(this).css(
+		"color", "#fffaf0" );
+	});
+   
+});
