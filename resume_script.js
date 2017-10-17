1c0436b0ff02148586fe6ba7a31526b744017d60
Ann Fang <32095875+e24033150@users.noreply.github.com>
2017-10-17 21:53:22 +0800

    Update resume_script.js

--- resume_script.js
+++ resume_script.js
@@ -8,5 +8,14 @@ $(function() {
 		$(this).css(
 		"color", "#fffaf0" );
 	});
-   
+   	
+	$('a').on('mouseover', function() {
+		$(this).css(
+		"color","#FF00FF" );
+	});
+	$('a').on('mouseout', function(){
+		$(this).css(
+		"color", "#8B008B" );
+	});
+	
 });
