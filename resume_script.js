c3d7bbe5ede528a294dc14407259136c177f9e79
Ann Fang <32095875+e24033150@users.noreply.github.com>
2017-10-17 23:35:30 +0800

    Update resume_script.js

--- resume_script.js
+++ resume_script.js
@@ -18,4 +18,46 @@ $(function() {
 		"color", "#8B008B" );
 	});
 	
+		var t=0;
+	var str1 = $('#animate1').text();
+	var i = 0;
+	var typing_inst1 = setInterval(typing1, 100);
+	function typing1()
+	{
+		if(i < str1.length -1)
+		{
+			$('#animate1').text(str1.substr(0,i) + "_");
+			i++;
+		}
+		else if(i == str1.length-1)
+		{
+			$('#animate1').text(str1);
+			t=1;
+			$('#animate2').show();
+			clearInterval(typing_inst1);
+		}
+	}
+	
+	
+	var str2 = $('#animate2').text();
+	var j = 0;
+	var typing_inst2 = setInterval(typing2, 100);
+	function typing2()
+	{
+		if(t==0)
+		{
+			$('#animate2').hide();
+		}
+		else if(j < str2.length -1)
+		{
+			$('#animate2').text(str2.substr(0,j) + "_");
+			j++;
+		}
+		else if(j == str2.length-1)
+		{
+			$('#animate2').text(str2);
+			clearInterval(typing_inst2);
+		}
+	}
+	
 });
